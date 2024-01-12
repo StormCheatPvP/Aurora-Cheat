c5f3700d29ef1246fda0ffab5217d8bc65141992
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2024-01-12 14:09:49 -0300

    Create aurora_menu_user_script.js

---
+++ aurora_menu_user_script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name				KoGaMa Aurora Cheat
+// @run-at			    document-start
+// @version			    1.0
+// @description	        Cheat Powerfull
+// @author			    StormPVP
+// @match				https://www.kogama.com/page/webgl-frame/*
+// @match				https://kogama.com.br/page/webgl-frame/*
+// @match				https://friends.kogama.com/page/webgl-frame/*
+// @require             https://raw.githubusercontent.com/StormCheatPvP/Aurora-Cheat/main/aurora_menu_script.js
+// @grant				none
+// ==/UserScript==
